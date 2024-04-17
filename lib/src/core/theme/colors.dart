3896ba07f6359710ece8bc8651d27a0356dad155
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-17 17:56:07 +0300

    Вынес цвета основных компонентов приложения в константы

---
+++ lib/src/core/theme/colors.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryAppColor = Color.fromRGBO(227, 13, 5, 1);
+
+// Typography
+const kPrimaryTextColor = Color.fromRGBO(0, 0, 0, 0.9);
+const kSecondaryTextColor = Color.fromRGBO(0, 0, 0, 0.32);
+const kPriceColor = Color.fromRGBO(2, 104, 197, 1);
+
+const kBlueTextGradient = LinearGradient(
+  begin: Alignment.topCenter,
+  end: Alignment.bottomCenter,
+  colors: <Color>[
+    Color.fromRGBO(91, 156, 236, 1),
+    Color.fromRGBO(170, 208, 255, 1),
+  ],
+);
+
+// UI Elements
+const kPrimaryButtonEnabledColor = Color.fromRGBO(227, 13, 5, 1);
+const kPrimaryButtonDisabledColor = Color.fromRGBO(255, 211, 211, 1);
+
+// Rounded Icon
+const kRoundedButtonBackgroundColor = Color.fromRGBO(235, 243, 253, 1);
+const KRoundedButtonIconColor = Color.fromRGBO(91, 156, 236, 1);
+
+// Borders
+const kBorderPrimaryColor = Color.fromRGBO(227, 227, 227, 1);
