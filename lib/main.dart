18d93cc458556ece11a4a8aa338bf256627220c8
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-19 00:33:34 +0300

    Небольшой рефактор main файла

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:frifri/src/feature/application/initialization/runner/runner.dart';
 
 void main() {
-  runZonedGuarded(() async {
-    await Runner.startApplication();
-  }, (error, stack) {
-    debugPrintStack(stackTrace: stack, label: error.toString());
-  });
+  runZonedGuarded(
+    () async {
+      await Runner.startApplication();
+    },
+    (error, stack) {
+      debugPrintStack(
+        stackTrace: stack,
+        label: error.toString(),
+      );
+    },
+  );
 }
