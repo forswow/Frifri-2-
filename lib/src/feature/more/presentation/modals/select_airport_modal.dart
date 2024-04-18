70503fc12c95022551956303ea90c6a8bf5bd0e5
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-18 14:47:52 +0300

    Рефактор выбора аэропорта на новый виджет RoundedListContainer

--- lib/src/feature/more/presentation/modals/select_airport_modal.dart
+++ lib/src/feature/more/presentation/modals/select_airport_modal.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:frifri/src/core/theme/colors.dart';
+import 'package:frifri/src/core/ui-kit/buttons/selection_item.dart';
 
-import 'package:frifri/src/core/ui-kit/painters/custom_checkbox.dart';
 import 'package:frifri/src/feature/more/presentation/modals/base_modal.dart';
+import 'package:frifri/src/feature/more/presentation/widgets/rounded_list_container.dart';
 
 class SelectAirportModal extends BottomSheetModalBase {
   const SelectAirportModal({super.key});
@@ -34,55 +33,13 @@ class _AirportSelectionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: const BorderRadius.all(
-          Radius.circular(12.0),
-        ),
-        border: Border.all(
-          color: kBorderPrimaryColor,
-          width: 1,
-        ),
-      ),
-      margin: const EdgeInsets.all(24.0),
-      child: Column(
-        children: [
-          GestureDetector(
-            onTap: () {},
-            child: _AirportSelectionItem(airportTitle: "Тбилиси"),
-          ),
-          defaultListDivider,
-          _AirportSelectionItem(airportTitle: "Кутаиси"),
-          defaultListDivider,
-          _AirportSelectionItem(airportTitle: "Батуми")
-        ],
-      ),
-    );
-  }
-}
-
-class _AirportSelectionItem extends StatelessWidget {
-  const _AirportSelectionItem({required this.airportTitle, super.key});
-
-  final String airportTitle;
-
-  static const defaultTextStyle = TextStyle(
-    fontSize: 14,
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: const EdgeInsets.only(
-        left: 12,
-        right: 0,
-      ),
-      minLeadingWidth: 0,
-      title: Text(
-        airportTitle,
-        style: defaultTextStyle,
-      ),
-      leading: const CustomCheckBox(),
+    return RoundedListContainer(
+      separator: defaultListDivider,
+      children: [
+        SelectionItemTile(title: "Тбилиси"),
+        SelectionItemTile(title: "Кутаиси"),
+        SelectionItemTile(title: "Батуми"),
+      ],
     );
   }
 }
