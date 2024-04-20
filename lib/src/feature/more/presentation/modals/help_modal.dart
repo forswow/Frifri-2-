eb6602e26e8ce0bef168e5f84223a8063468bfc6
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-21 00:13:21 +0300

    Модалка помощи

---
+++ lib/src/feature/more/presentation/modals/help_modal.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:frifri/src/feature/more/presentation/modals/base_modal.dart';
+import 'package:frifri/src/feature/more/presentation/modals/faq_modal.dart';
+import 'package:frifri/src/feature/more/presentation/widgets/rounded_list_container.dart';
+
+class HelpModal extends BottomSheetModalBase {
+  const HelpModal({super.key});
+
+  static const TextStyle _listTileTextStyle = TextStyle(
+    fontSize: 16.0,
+  );
+
+  static const _defaultListDivider = Divider(
+    height: 1,
+    thickness: 0.5,
+    indent: 16,
+  );
+
+  @override
+  Widget build_header(BuildContext context) {
+    return DefaultModalHeader(
+      centerText: "Помощь",
+    );
+  }
+
+  @override
+  Widget build_content(BuildContext context) {
+    return Container(
+      child: RoundedListContainer(
+        separator: _defaultListDivider,
+        children: [
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => FaqModal(),
+              );
+            },
+            child: ListTile(
+              contentPadding: EdgeInsets.only(left: 16, right: 12),
+              title: Text(
+                "Самые частые вопросы",
+                style: _listTileTextStyle,
+              ),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => FaqModal(),
+              );
+            },
+            child: ListTile(
+              contentPadding: EdgeInsets.only(left: 16, right: 12),
+              title: Text(
+                "Отмена рейсов",
+                style: _listTileTextStyle,
+              ),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+              ),
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              showModalBottomSheet(
+                context: context,
+                isScrollControlled: true,
+                builder: (context) => FaqModal(),
+              );
+            },
+            child: ListTile(
+              contentPadding: EdgeInsets.only(left: 16, right: 12),
+              title: Text(
+                "Оформление билета",
+                style: _listTileTextStyle,
+              ),
+              trailing: Icon(
+                Icons.keyboard_arrow_right,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
