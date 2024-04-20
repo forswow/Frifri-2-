267da70c2d8c8de217df0adac0d2e32cf7a94afe
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-20 23:09:27 +0300

    Смена названия faq_help_modal -> faq_modal, т.к. помощь это по сути отдельная модалка

--- lib/src/feature/more/presentation/screens/more_screen.dart
+++ lib/src/feature/more/presentation/screens/more_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:frifri/src/feature/more/presentation/modals/faq_help_modal.dart';
+import 'package:frifri/src/feature/more/presentation/modals/faq_modal.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
