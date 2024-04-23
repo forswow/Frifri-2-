73bbff513601754f4b795c782fc7f6cdf412006a
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-24 01:01:43 +0300

    ⚠️ Финал модалки выбора языка
    Интересная заметка: теперь, когда есть одна общая кнопка подтверждения для группы настроек, и когда её активность и работа зависит от этой группы настроек, виджет очень сложно декомпозировать.
    
    Именно по этой причине я решил объединить список и модалку в один StatefulWidget с состоянием, в остальных случаях, когда таких состояний много придётся что-то делать для поднятия состояния вверх. Либо локальный Inherited Widget, либо что-то аналогичное, возможно ChangeNotifier.
    
    Либо держать все стейты внутри модалки, а onChange callback-и отдельных виджетов передавать в них через параметр конструктора.
    
    Наверняка должен быть лучший способ это сделать, но на данный момент не могу его обнаружить.

--- lib/src/feature/more/presentation/modals/select_language_modal.dart
+++ lib/src/feature/more/presentation/modals/select_language_modal.dart
@@ -1,14 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:frifri/src/core/ui-kit/buttons/confirm_button.dart';
+
 import 'package:frifri/src/core/ui-kit/modals/default_modal_header.dart';
 import 'package:frifri/src/feature/more/domain/language_bloc.dart';
 
 import 'package:frifri/src/core/ui-kit/modals/base_modal.dart';
 import 'package:frifri/src/feature/more/presentation/widgets/custom_radio_list.dart';
 import 'package:frifri/src/feature/more/presentation/widgets/rounded_list_container.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class SelectLanguageModal extends BottomSheetStatelessModalBase {
-  const SelectLanguageModal({super.key});
+class SelectLanguageModal extends BottomSheetStatefulModalBase {
+  @override
+  State<BottomSheetStatefulModalBase> createState() {
+    return SelectLanguageModalState();
+  }
+}
+
+class SelectLanguageModalState extends BottomSheetStatefulModalBaseState {
+  static const _contentPadding = 24.0;
+  static const _defaultListDivider = Divider(
+    height: 1,
+    thickness: 0.5,
+    indent: 44,
+  );
+
+  late String initialLanguage;
+  late String selectedLanguage;
+  bool isConfirmButtonActive = false;
+
+  @override
+  void initState() {
+    super.initState();
+    initialLanguage = context.read<AppLanguageCubit>().state;
+    selectedLanguage = initialLanguage;
+  }
 
   @override
   Widget build_header(BuildContext context) {
@@ -19,66 +47,62 @@ class SelectLanguageModal extends BottomSheetStatelessModalBase {
 
   @override
   Widget build_content(BuildContext context) {
-    return _LanguageSelectionList();
-  }
-}
-
-class _LanguageSelectionList extends StatefulWidget {
-  const _LanguageSelectionList({super.key});
-
-  static const _defaultListDivider = Divider(
-    height: 1,
-    thickness: 0.5,
-    indent: 44,
-  );
-
-  @override
-  State<_LanguageSelectionList> createState() => _LanguageSelectionListState();
-}
-
-class _LanguageSelectionListState extends State<_LanguageSelectionList> {
-  late String currentLanguage;
-
-  @override
-  void initState() {
-    super.initState();
-    currentLanguage = context.read<AppLanguageCubit>().state;
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return RoundedListContainer(
-      separator: _LanguageSelectionList._defaultListDivider,
+      separator: _defaultListDivider,
       children: [
         CustomRadioListTile(
           value: "ENG",
           title: Text("English"),
-          groupValue: currentLanguage,
+          groupValue: selectedLanguage,
           onChanged: (newValue) {
             setState(() {
-              currentLanguage = newValue!;
-              context
-                  .read<AppLanguageCubit>()
-                  .selectNewLanguage(currentLanguage);
+              selectedLanguage = newValue!;
+              isConfirmButtonActive = selectedLanguage != initialLanguage;
             });
           },
         ),
         CustomRadioListTile(
           value: "GEO",
           title: Text("Georgian"),
-          groupValue: currentLanguage,
+          groupValue: selectedLanguage,
           onChanged: (newValue) {
             setState(
               () {
-                currentLanguage = newValue!;
-                context
-                    .read<AppLanguageCubit>()
-                    .selectNewLanguage(currentLanguage);
+                selectedLanguage = newValue!;
+                isConfirmButtonActive = selectedLanguage != initialLanguage;
               },
             );
           },
         ),
       ],
     );
+  }
+
+  @override
+  List<Widget> build_overlay(BuildContext context) {
+    return [
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width - _contentPadding * 2,
+          child: ConfirmationButton(
+            child: Text(
+              AppLocalizations.of(context)!.confirm,
+              style: GoogleFonts.inter(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: isConfirmButtonActive
+                ? () {
+                    context
+                        .read<AppLanguageCubit>()
+                        .selectNewLanguage(selectedLanguage);
+                    context.pop();
+                  }
+                : null,
+          ),
+        ),
+      ),
+    ];
   }
 }
