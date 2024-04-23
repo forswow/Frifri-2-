eacd5c28cda2e2523f16c4f374a83972b059078d
Roman Berezkin <crazzzy_coder@mail.ru>
2024-04-24 00:31:07 +0300

    Зафиналил модалку настроек, теперь коммит настроек подтверждается по кнопке,
    Кнопка также реактивно реагирует на изменение настроек, благодаря наличию стейта в модалке.
    
    Кнопка реагирует по ощущениям с еле заметной задержкой, нужно попробовать это оптимизировать по возможности.

--- lib/src/feature/more/presentation/modals/settings_modal.dart
+++ lib/src/feature/more/presentation/modals/settings_modal.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:frifri/src/core/theme/colors.dart';
+import 'package:frifri/src/core/ui-kit/buttons/confirm_button.dart';
+import 'package:frifri/src/core/ui-kit/modals/default_modal_header.dart';
 import 'package:frifri/src/feature/more/domain/settings_bloc.dart';
-import 'package:frifri/src/feature/more/presentation/modals/base_modal.dart';
+import 'package:frifri/src/core/ui-kit/modals/base_modal.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class SettingsModal extends BottomSheetModalBase {
-  const SettingsModal({super.key});
+class SettingsModal extends BottomSheetStatefulModalBase {
+  @override
+  State<BottomSheetStatefulModalBase> createState() {
+    return SettingsModalState();
+  }
+}
 
+class SettingsModalState extends BottomSheetStatefulModalBaseState {
   @override
   Widget build_header(BuildContext context) {
     return DefaultModalHeader(
@@ -18,35 +27,81 @@ class SettingsModal extends BottomSheetModalBase {
 
   static const _contentPadding = 24.0;
 
+  late bool initialPushNotificationEnabled;
+  late bool pushNotificationEnabled;
+
+  late bool commitButtonEnabled;
+
+  @override
+  void initState() {
+    super.initState();
+    initialPushNotificationEnabled =
+        context.read<PushNotificationCubit>().state;
+    pushNotificationEnabled = initialPushNotificationEnabled;
+
+    commitButtonEnabled =
+        initialPushNotificationEnabled != pushNotificationEnabled;
+  }
+
   @override
   Widget build_content(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(_contentPadding),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(
-            AppLocalizations.of(context)!.pushNotification,
-            style: GoogleFonts.poppins(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
+    return IntrinsicHeight(
+      child: Container(
+        padding: EdgeInsets.all(_contentPadding),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.pushNotification,
+              style: GoogleFonts.poppins(
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          SizedBox(
-            child: BlocBuilder<PushNotificationCubit, bool>(
-              builder: (context, state) {
-                return CupertinoSwitch(
-                  value: state,
-                  onChanged: (value) {
-                    context.read<PushNotificationCubit>().setPushEnabled(value);
-                  },
-                  activeColor: kPrimaryAppColor,
-                );
-              },
+            SizedBox(
+              child: CupertinoSwitch(
+                value: pushNotificationEnabled,
+                onChanged: (value) {
+                  setState(() {
+                    pushNotificationEnabled = value;
+                    commitButtonEnabled = initialPushNotificationEnabled !=
+                        pushNotificationEnabled;
+                  });
+                },
+                activeColor: kPrimaryAppColor,
+              ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
+  }
+
+  @override
+  List<Widget> build_overlay(BuildContext context) {
+    return [
+      Align(
+        alignment: Alignment.bottomCenter,
+        child: SizedBox(
+          width: MediaQuery.sizeOf(context).width - _contentPadding * 2,
+          child: ConfirmationButton(
+            child: Text(
+              AppLocalizations.of(context)!.confirm,
+              style: GoogleFonts.inter(
+                color: Colors.white,
+              ),
+            ),
+            onPressed: commitButtonEnabled
+                ? () {
+                    context
+                        .read<PushNotificationCubit>()
+                        .setPushEnabled(pushNotificationEnabled);
+                    context.pop();
+                  }
+                : null,
+          ),
+        ),
+      ),
+    ];
   }
 }
