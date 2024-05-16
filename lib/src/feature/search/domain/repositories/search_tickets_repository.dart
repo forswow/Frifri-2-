532df715a2b995ab3dd45b59aaaccc4a74b8210c
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-05-16 23:58:22 +0300

    Удалил старые неиспользуемые файлы

--- lib/src/feature/search/domain/repositories/search_tickets_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:frifri/src/feature/search/domain/dto/search_dto.dart';
-
-import '../entities/flight_search_result_entity.dart';
-
-abstract interface class SearchTicketRepository {
-  Future<FlightSearchResultEntity> search(SearchDto searchDto);
-}
