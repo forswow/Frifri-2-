b36fcef5df79a5060ef194fc4f581388dbc45bdb
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-05-20 09:25:23 +0300

    Возврат скрина

--- lib/src/feature/application/root_app/presentation/widgets/app.dart
+++ lib/src/feature/application/root_app/presentation/widgets/app.dart
@@ -4,14 +4,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frifri/src/core/theme/colors.dart';
 import 'package:frifri/src/core/theme/theme.dart';
 import 'package:frifri/src/feature/application/navigation/navigation_manager.dart';
-import 'package:frifri/src/feature/buy_ticket/presentation/screens/search_ticket_form_screen.dart';
 import 'package:frifri/src/feature/more/domain/language_bloc.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:frifri/src/module/country_search/presentation/screens/country_search_screen.dart';
-
-import '../../../../buy_ticket/presentation/modals/search_modal_fly_to/components/emptystring_list.dart';
-import '../../../../buy_ticket/presentation/modals/search_modal_fly_to/searchfly_modal_to.dart';
 
 /// {@template app}
 /// App widget.
@@ -28,14 +23,14 @@ class App extends StatelessWidget {
       ),
       child: BlocBuilder<AppLanguageCubit, String>(
         builder: (context, state) {
-          return MaterialApp(
+          return MaterialApp.router(
             locale: Locale(state),
             localizationsDelegates: AppLocalizations.localizationsDelegates,
             supportedLocales: AppLocalizations.supportedLocales,
             // locale:
             debugShowCheckedModeBanner: false,
             theme: theme,
-            home: CountrySearchScreen(),
+            routerConfig: NavigationManager.router,
             // routerConfig: NavigationManager.router,
           );
         },
