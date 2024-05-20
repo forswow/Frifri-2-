f8daef8bbd141dbc4f50f04b9660e7a26917d185
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-05-20 21:37:45 +0300

    Форматирование

--- lib/src/feature/buy_ticket/presentation/modals/ticket_modal/path_info_transfer.dart
+++ lib/src/feature/buy_ticket/presentation/modals/ticket_modal/path_info_transfer.dart
@@ -7,32 +7,33 @@ class PathInfoTransfer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+      alignment: Alignment.center,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(6),
+        color: const Color(0xFFF8F8F8),
+      ),
+      child: Stack(
         alignment: Alignment.center,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(6),
-          color: const Color(0xFFF8F8F8),
-        ),
-        child: Stack(
-          alignment: Alignment.center,
-          children: <Widget>[
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text('Пересадка\nВ Стамбуле'),
-                  ],
-                ),
-                Text('3h 55m'),
-              ],
-            ),
-            Positioned(
-              child: SvgPicture.asset('assets/icons/walking.svg'),
-            )
-          ],
-        ));
+        children: <Widget>[
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Пересадка\nВ Стамбуле'),
+                ],
+              ),
+              Text('3h 55m'),
+            ],
+          ),
+          Positioned(
+            child: SvgPicture.asset('assets/icons/walking.svg'),
+          )
+        ],
+      ),
+    );
   }
 }
