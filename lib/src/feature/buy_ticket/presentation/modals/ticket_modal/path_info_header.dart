f6b4a4705b2e302257a45ca8782340741a76b602
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-05-27 18:57:58 +0300

    Убрана кнопка подробнее из SegmentInfoHeader
    По желанию клиента, т.к. нет дизайна для страницы этой кнопки и она фактически фиктивная.

--- lib/src/feature/buy_ticket/presentation/modals/ticket_modal/path_info_header.dart
+++ lib/src/feature/buy_ticket/presentation/modals/ticket_modal/path_info_header.dart
@@ -44,23 +44,6 @@ class SegmentInfoHeader extends StatelessWidget {
             )
           ],
         ),
-        InkWell(
-          onTap: () {},
-          child: Container(
-            padding: const EdgeInsets.fromLTRB(16, 6, 16, 6),
-            decoration: const BoxDecoration(
-              color: Color(0xFFEEF3FD),
-              borderRadius: BorderRadius.all(Radius.circular(100)),
-            ),
-            child: Text(
-              "Подробнее",
-              style: AppStyles.textStylePoppins.copyWith(
-                fontWeight: FontWeight.bold,
-                fontSize: 12,
-              ),
-            ),
-          ),
-        )
       ],
     );
   }
