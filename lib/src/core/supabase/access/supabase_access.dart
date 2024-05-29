7b3ec0bb3aa109c3a1e0575f9dd7232c3540b853
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-05-30 01:34:39 +0300

    Unused import

--- lib/src/core/supabase/access/supabase_access.dart
+++ lib/src/core/supabase/access/supabase_access.dart
@@ -1,10 +1,6 @@
-import 'package:supabase_flutter/supabase_flutter.dart';
-
 abstract final class SupaBaseAccess {
   static const anonKey =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlqdXZmdXl2dHZqZXFwcW5yY21yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTY2MzI4ODUsImV4cCI6MjAzMjIwODg4NX0.q6pIyYpLlMEzUy5xcJ5j1AdoDF0ag5T-LVITzPk_LUw';
 
   static const url = 'https://ijuvfuyvtvjeqpqnrcmr.supabase.co';
 }
-
-
