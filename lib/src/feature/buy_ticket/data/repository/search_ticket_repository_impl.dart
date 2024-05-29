4aac07c02c0c846e6bd6471dc59c2abd09284d90
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-05-29 20:41:55 +0300

    Вынос entities из блока в repository для поиска билетов

--- lib/src/feature/buy_ticket/data/repository/search_ticket_repository_impl.dart
+++ lib/src/feature/buy_ticket/data/repository/search_ticket_repository_impl.dart
@@ -1,10 +1,13 @@
 import 'package:frifri/src/feature/buy_ticket/data/DTO/search_tickets.dart';
-import 'package:frifri/src/feature/buy_ticket/data/DTO/search_tickets_result.dart';
+import 'package:frifri/src/feature/buy_ticket/data/data_sources/avia_tickets_api_client.dart';
 import 'package:frifri/src/feature/buy_ticket/data/data_sources/search/search_data_sources.dart';
 import 'package:frifri/src/feature/buy_ticket/data/dto/month_matrix.dart';
+import 'package:frifri/src/feature/buy_ticket/domain/entities/airport_entity.dart';
 import 'package:frifri/src/feature/buy_ticket/domain/entities/booking_ticket_entity.dart';
+import 'package:frifri/src/feature/buy_ticket/domain/entities/ticket_entity.dart';
 
 import 'package:frifri/src/feature/buy_ticket/domain/repository/search_tickets_repo.dart';
+import 'package:intl/intl.dart';
 
 import '../dto/ticket_search_query.dart';
 
@@ -25,9 +28,102 @@ base class SearchTicketRepoImpl implements ISearchTicketsRepo {
   }
 
   @override
-  Future<TicketsSearchResultBySearchId> getTicketsBySearchId(
-      {required String searchId}) async {
-    return await searchDataSources.getTicketsBySearchId(searchId);
+  Future<List<TicketEntity>> getTicketsBySearchId({
+    required String searchId,
+    required AirportEntity originAirport,
+    required AirportEntity destinationAirport,
+    required String currency,
+    required String locale,
+    required Map<String, double> currencyRates,
+  }) async {
+    final List<TicketEntity> newTickets = [];
+
+    final result = await searchDataSources.getTicketsBySearchId(searchId);
+
+    // Bang операторы используются по причине того, что
+    // API всегда содержит вместе с собой airlines мапу
+    // необходимую для получения названия самолёта/аэролинии
+    // и она гарантировано содержит IATA код для каждого proposal
+
+    // Сами же airports, proposals, airlines являются nullable
+    // то есть апи может их и вовсе не прислать
+    //
+    // А пришлёт она только 1 элемент в массиве, который будет содержать
+    // searchId, из которого собрать чанк нельзя
+    while (result.data.length != 1) {
+      for (final chunk in result.data) {
+        for (final proposal in chunk.proposals) {
+          final List<SegmentEntity> ticketSegments = [];
+
+          for (var proposalSegment in proposal.segments.first.flight) {
+            final segmentDuration = proposalSegment.duration;
+
+            ticketSegments.add(
+              SegmentEntity(
+                airlineLogo: getAirlineLogoUrl(
+                  proposalSegment.operatedByAirlineIataCode,
+                  "200/200",
+                ),
+                airlineName: chunk
+                    .airlines[proposalSegment.operatedByAirlineIataCode]!.name,
+                arrivalAirportName:
+                    chunk.airports[proposalSegment.arrivalAt]!.name,
+                arrivalCityName:
+                    chunk.airports[proposalSegment.arrivalAt]!.city,
+                departureAirportName:
+                    chunk.airports[proposalSegment.departureAt]!.name,
+                departureCityName:
+                    chunk.airports[proposalSegment.departureAt]!.city,
+                departureDate: DateTime.parse(proposalSegment.departureDate),
+                departureTime: proposalSegment.departureTime,
+                arrivalDate: DateTime.parse(proposalSegment.arrivalDate),
+                arrivalTime: proposalSegment.arrivalTime,
+                segmentFormattedDuration:
+                    "${segmentDuration ~/ 60}h ${segmentDuration % 60}m",
+                arrivalTimestamp: proposalSegment.arrivalTimestamp,
+                departureTimestamp: proposalSegment.departureTimestamp,
+              ),
+            );
+          }
+
+          final priceInRubles = proposal.terms.priceInRubles;
+          final currencyRate = currencyRates[currency]!;
+
+          final priceFormatter = NumberFormat.simpleCurrency(
+            decimalDigits: 0,
+            name: currency.toUpperCase(),
+            locale: locale.toUpperCase(),
+          );
+
+          final ticket = TicketEntity(
+            originAirport: originAirport,
+            destinationAirport: destinationAirport,
+            flightDuration:
+                "${proposal.totalDurationInMinutes ~/ 60}h ${proposal.totalDurationInMinutes % 60}m",
+            segmentsList: ticketSegments,
+            departureTime: ticketSegments.first.departureTime,
+            arrivalTime: ticketSegments.last.arrivalTime,
+
+            formattedPrice: priceFormatter
+                .format(
+                  priceInRubles ~/ currencyRate,
+                )
+                .replaceAll(",", " "),
+            price: priceInRubles ~/ currencyRate,
+
+            isDirect: proposal.isDirect,
+
+            // For forming url for booking
+            searchId: searchId,
+            termsUrl: proposal.terms.urlCode,
+          );
+
+          newTickets.add(ticket);
+        }
+      }
+    }
+
+    return newTickets;
   }
 
   @override
--- lib/src/feature/buy_ticket/presentation/bloc/search_tickets/search_ticket_bloc.dart
+++ lib/src/feature/buy_ticket/presentation/bloc/search_tickets/search_ticket_bloc.dart
@@ -4,7 +4,6 @@ import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:frifri/src/core/utils/logger.dart';
-import 'package:frifri/src/feature/buy_ticket/data/data_sources/avia_tickets_api_client.dart';
 import 'package:frifri/src/feature/buy_ticket/data/dto/ticket_search_query.dart';
 import 'package:frifri/src/feature/buy_ticket/domain/entities/ticket_entity.dart';
 import 'package:frifri/src/feature/buy_ticket/domain/entities/trip_class.dart';
@@ -12,7 +11,6 @@ import 'package:frifri/src/feature/buy_ticket/domain/repository/search_tickets_r
 import 'package:frifri/src/feature/buy_ticket/presentation/bloc/search_tickets/search_ticket_bloc_events.dart';
 import 'package:frifri/src/feature/buy_ticket/presentation/bloc/search_tickets/search_ticket_bloc_states.dart';
 import 'package:frifri/src/feature/buy_ticket/presentation/screens/search_ticket_form_screen.dart';
-import 'package:intl/intl.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   final ISearchTicketsRepo ticketRepo;
@@ -29,12 +27,10 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
     super.onChange(change);
   }
 
-  // TODO: Перенести логику сборки модели в репозиторий + стримы
+  // TODO Реализация на стримах
   FutureOr<void> _onStartSearchTicketEvent(
       StartSearchTicketEvent event, Emitter<SearchState> emit) async {
     emit(SearchingInProgress(tickets: const []));
-
-    final List<TicketEntity> newTickets = List.empty(growable: true);
 
     try {
       final searchModel = event.searchModelForm;
@@ -53,121 +49,14 @@ class SearchBloc extends Bloc<SearchEvent, SearchState> {
       if (emit.isDone) return;
 
       logger.i("SearchTicketsBloc: Sent search request");
-      var result = await ticketRepo.getTicketsBySearchId(searchId: searchId);
-
-      // Bang операторы используются по причине того, что
-      // API всегда содержит вместе с собой airlines мапу
-      // необходимую для получения названия самолёта/аэролинии
-      // и она гарантировано содержит IATA код для каждого proposal
-
-      // Сами же airports, proposals, airlines являются nullable
-      // то есть апи может их и вовсе не прислать
-      //
-      // А пришлёт она только 1 элемент в массиве, который будет содержать
-      // searchId, из которого собрать чанк нельзя
-      while (result.data.length != 1) {
-        for (final chunk in result.data) {
-          for (final proposal in chunk.proposals) {
-            final List<SegmentEntity> ticketSegments = [];
-
-            for (var proposalSegment in proposal.segments.first.flight) {
-              final departureAirportName =
-                  chunk.airports[proposalSegment.departureAt]!.name;
-              final departureCityName =
-                  chunk.airports[proposalSegment.departureAt]!.city;
-
-              final arrivalAirportName =
-                  chunk.airports[proposalSegment.arrivalAt]!.name;
-              final arrivalCityName =
-                  chunk.airports[proposalSegment.arrivalAt]!.city;
-
-              final airlineName = chunk
-                  .airlines[proposalSegment.operatedByAirlineIataCode]!.name;
-
-              final departureDate =
-                  DateTime.parse(proposalSegment.departureDate);
-              final departureTime = proposalSegment.departureTime;
-
-              final arrivalDate = DateTime.parse(proposalSegment.arrivalDate);
-              final arrivalTime = proposalSegment.arrivalTime;
-
-              final segmentDuration = proposalSegment.duration;
-              final segmentFormattedDuration =
-                  "${segmentDuration ~/ 60}h ${segmentDuration % 60}m";
-
-              final departureTimestamp = proposalSegment.departureTimestamp;
-              final arrivalTimestamp = proposalSegment.arrivalTimestamp;
-
-              ticketSegments.add(
-                SegmentEntity(
-                  airlineLogo: getAirlineLogoUrl(
-                    proposalSegment.operatedByAirlineIataCode,
-                    "200/200",
-                  ),
-                  airlineName: airlineName,
-                  arrivalAirportName: arrivalAirportName,
-                  arrivalCityName: arrivalCityName,
-                  departureAirportName: departureAirportName,
-                  departureCityName: departureCityName,
-                  departureDate: departureDate,
-                  departureTime: departureTime,
-                  arrivalDate: arrivalDate,
-                  arrivalTime: arrivalTime,
-                  segmentFormattedDuration: segmentFormattedDuration,
-                  arrivalTimestamp: arrivalTimestamp,
-                  departureTimestamp: departureTimestamp,
-                ),
-              );
-            }
-
-            final formattedDuration =
-                "${proposal.totalDurationInMinutes ~/ 60}h ${proposal.totalDurationInMinutes % 60}m";
-
-            final priceInRubles = proposal.terms.priceInRubles;
-            final currencyRate = currencyRates[event.currency]!;
-            final finalPrice = priceInRubles ~/ currencyRate;
-
-            final priceFormatter = NumberFormat.simpleCurrency(
-              decimalDigits: 0,
-              name: event.currency.toUpperCase(),
-              locale: event.locale.toUpperCase(),
-            );
-
-            final formattedTicketPrice = priceFormatter
-                .format(
-                  finalPrice,
-                )
-                .replaceAll(",", " ");
-
-            final ticket = TicketEntity(
-              originAirport: searchModel.departureAt!,
-              destinationAirport: searchModel.arrivalAt!,
-              flightDuration: formattedDuration,
-              segmentsList: ticketSegments,
-              departureTime: ticketSegments.first.departureTime,
-              arrivalTime: ticketSegments.last.arrivalTime,
-
-              formattedPrice: formattedTicketPrice,
-              price: finalPrice,
-
-              isDirect: proposal.isDirect,
-
-              // For forming url for booking
-              searchId: searchId,
-              termsUrl: proposal.terms.urlCode,
-            );
-
-            newTickets.add(ticket);
-          }
-        }
-
-        await Future.delayed(const Duration(seconds: 5));
-
-        // logger.i("Requesting next chunk...");
-        // result = await ticketRepo.getTicketsBySearchId(searchId: searchId);
-        // Дедлайн...
-        break;
-      }
+      var newTickets = await ticketRepo.getTicketsBySearchId(
+        searchId: searchId,
+        originAirport: event.searchModelForm.departureAt!,
+        destinationAirport: event.searchModelForm.arrivalAt!,
+        currency: event.currency,
+        locale: event.locale,
+        currencyRates: currencyRates,
+      );
 
       sortTicketsByPrice(newTickets);
       emit(SearchComplete(tickets: newTickets));
