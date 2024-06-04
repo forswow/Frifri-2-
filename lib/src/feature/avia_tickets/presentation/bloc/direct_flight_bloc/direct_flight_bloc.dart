dd30fbcc832c1208f8b5de79f2a6d36e5ba8c5ec
Glitchy-Sheep <crazzzy_coder@mail.ru>
2024-06-04 15:29:53 +0300

    Сортировка импортов

--- lib/src/feature/avia_tickets/presentation/bloc/direct_flight_bloc/direct_flight_bloc.dart
+++ lib/src/feature/avia_tickets/presentation/bloc/direct_flight_bloc/direct_flight_bloc.dart
@@ -1,14 +1,11 @@
 import 'dart:async';
 
-import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:frifri/src/core/utils/logger.dart';
-import 'package:frifri/src/feature/avia_tickets/presentation/bloc/direct_flight_bloc/direct_flight_bloc_event.dart';
-import 'package:frifri/src/feature/avia_tickets/presentation/bloc/direct_flight_bloc/direct_flight_bloc_state.dart';
-import 'package:frifri/src/feature/shared/data/dto/prices_for_dates.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-import 'package:frifri/src/feature/shared/data/dto/month_matrix.dart';
+import 'package:dio/dio.dart';
+import 'package:frifri/src/feature/avia_tickets/presentation/bloc/direct_flight_bloc/direct_flight_bloc_event.dart';
+import 'package:frifri/src/feature/avia_tickets/presentation/bloc/direct_flight_bloc/direct_flight_bloc_state.dart';
 import 'package:frifri/src/feature/avia_tickets/domain/repo/destination_country_repo.dart';
 
 class DirectFlightBloc extends Bloc<DirectFlightEvent, DirectFlightState> {
